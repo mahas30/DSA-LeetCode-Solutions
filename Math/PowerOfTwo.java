4ad80d07f4637a13faf7b7c6395b8dec807b3807
mahas30 <mahasenthilkumar0930@gmail.com>
2026-02-18 10:44:58 +0530

    Create PowerOfTwo.java
    Added LeetCode problem 231 - Power Of Two(Bit Manipulation)

---
+++ Math/PowerOfTwo.java
@@ -0,0 +1,18 @@
+/*
+Problem : Power of Two
+LeetCode : 231
+Difficulty : Easy
+
+Approach : A number is a power of 2 if has only one bit set.
+Therefore, solved using bitwise trick : 
+n>0 AND (n&(n-1))==0
+
+Time complexity : O(1)
+Space complexitiy : O(1)
+*/
+
+class Solution{
+    public boolean isPowerOfTwo(int n){
+        return n>0 && (n & (n-1))==0;
+    }
+}
